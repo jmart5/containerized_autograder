cb14f376c0295e4176059adaa92eb31fe98c67bd
jmart5 <93228623+jmart5@users.noreply.github.com>
2023-10-26 19:35:11 -0400

    Removed Directory

--- grading_directory/startup/run_grader.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Docker command to run the grader
-docker run -v "$(pwd)":/grading_dir -w /grading_dir -v "$(pwd)"/tests/testcases:/grading_dir/tests/testcases grader-oct21 /bin/bash -c "autograder run && cd / && python3 ./csv_generator.py"
-
